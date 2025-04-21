04bcbe22ed140fcd1e7203330dc62efb2847a10f
Maitena Chomicz <maitenac2003@gmail.com>
2025-04-21 20:14:35 -0300

    inclusión de .gitignore

--- config/mongo.ts
+++ config/mongo.ts
@@ -1,5 +1,17 @@
 import mongoose from "mongoose"
+process.loadEnvFile()
 
-const connectDB = () => {}
+const URI_DB = process.env.URI_DB || ""
+console.log(URI_DB)
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(URI_DB)
+        console.log("Conectado correctamente :)")
+    } catch (error) {
+        console.log("Problemas para conectarse :(")
+    }
+    
+}
 
 export { connectDB }
