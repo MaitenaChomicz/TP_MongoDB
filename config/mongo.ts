c16f12156eff497c69793552df791a8733bfe487
Maitena Chomicz <maitenac2003@gmail.com>
2025-04-22 16:53:03 -0300

    correción de archivos, conexión a mongoDB completada

--- config/mongo.ts
+++ config/mongo.ts
@@ -2,14 +2,13 @@ import mongoose from "mongoose"
 process.loadEnvFile()
 
 const URI_DB = process.env.URI_DB || ""
-console.log(URI_DB)
 
 const connectDB = async () => {
     try{
         await mongoose.connect(URI_DB)
         console.log("Conectado correctamente :)")
     } catch (error) {
-        console.log("Problemas para conectarse :(")
+        console.log("Problemas para conectarse :(", error)
     }
     
 }
--- index.ts
+++ index.ts
@@ -1,20 +1,18 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import movieRoutes from './routes/movies'
-import mongoose from 'mongoose'
-import { connectDB } from './database' // corregir por ./config/mongo.ts?
-connectDB()
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import movieRoutes from './routes/movies';
+import { connectDB } from './config/mongo';
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-
 app.use('/api/movies', movieRoutes)
-
-// tiene algun sentido la ubicacion ? connectDB() 
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
